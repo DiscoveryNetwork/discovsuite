60320f8101cab17ee945e04300216d5c9b41bb78
ParrotLync <43622266+ParrotLync@users.noreply.github.com>
2020-12-06 15:16:50 +0100

    Staff punching

--- src/main/java/nl/parrotlync/discovsuite/listener/EventListener.java
+++ src/main/java/nl/parrotlync/discovsuite/listener/EventListener.java
@@ -1,14 +1,24 @@
 package nl.parrotlync.discovsuite.listener;
 
 import nl.parrotlync.discovsuite.DiscovSuite;
+import org.apache.commons.lang.time.DateUtils;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.util.Vector;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.UUID;
 
 public class EventListener implements Listener {
+    private final HashMap<UUID, Date> cooldownMap = new HashMap<>();
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
@@ -16,6 +26,7 @@ public class EventListener implements Listener {
         if (event.getPlayer().hasPermission("discovsuite.fly.onjoin")) {
             event.getPlayer().setAllowFlight(true);
         }
+
     }
 
     @EventHandler
@@ -28,6 +39,22 @@ public class EventListener implements Listener {
         String[] lines = event.getLines();
         for (int i = 0; i < lines.length; i++) {
             event.setLine(i, ChatColor.translateAlternateColorCodes('&', event.getLine(i)));
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent event) {
+        if (event.getDamager().hasPermission("discovsuite.punch") && event.getDamager() instanceof Player) {
+            if (event.getEntity() instanceof Player) {
+                Player player = (Player) event.getEntity();
+                if (player.hasPermission("discovsuite.punchable")) {
+                    Date now = new Date();
+                    if (cooldownMap.get(event.getDamager().getUniqueId()) == null || now.compareTo(cooldownMap.get(event.getDamager().getUniqueId())) > 0) {
+                        cooldownMap.put(event.getDamager().getUniqueId(), DateUtils.addSeconds(now, 5));
+                        player.setVelocity(new Vector(0, 1, 0));
+                    }
+                }
+            }
         }
     }
 }
