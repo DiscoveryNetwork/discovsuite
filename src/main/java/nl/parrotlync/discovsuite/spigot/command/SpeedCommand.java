f0fd7c05d0f2cd6c80c25b44f6636ff48e320161
Alain Lardinois <43622266+ParrotLync@users.noreply.github.com>
2021-01-10 21:56:00 +0100

    Fix speed command

--- src/main/java/nl/parrotlync/discovsuite/spigot/command/SpeedCommand.java
+++ src/main/java/nl/parrotlync/discovsuite/spigot/command/SpeedCommand.java
@@ -2,15 +2,11 @@ package nl.parrotlync.discovsuite.spigot.command;
 
 import nl.parrotlync.discovsuite.spigot.util.ChatUtil;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
-import org.bukkit.util.StringUtil;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class SpeedCommand implements TabExecutor {
+public class SpeedCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -30,7 +26,7 @@ public class SpeedCommand implements TabExecutor {
             return true;
         }
 
-        float value = Math.max(0, Math.min(10, Float.parseFloat(args[1])));
+        float value = Math.max(0, Math.min(10, Float.parseFloat(args[0])));
         if (player.isFlying()) {
             player.setFlySpeed(value / 10);
             ChatUtil.sendConfigMessage(sender, "fly-speed-change", String.valueOf(value));
@@ -39,19 +35,5 @@ public class SpeedCommand implements TabExecutor {
             ChatUtil.sendConfigMessage(sender, "walk-speed-change", String.valueOf(value));
         }
         return true;
-    }
-
-    @Override
-    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-        List<String> suggestions = new ArrayList<>();
-
-
-        if (args.length == 1 && sender.hasPermission("discovsuite.speed")) {
-            suggestions.add("walk");
-            suggestions.add("fly");
-            return StringUtil.copyPartialMatches(args[0], suggestions, new ArrayList<>());
-        }
-
-        return suggestions;
     }
 }
