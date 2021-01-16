6a92aab005323955a914e9323b26837ede1e84d1
Alain Lardinois <43622266+ParrotLync@users.noreply.github.com>
2021-01-16 16:53:44 +0100

    Add configMessage with argument

--- src/main/java/nl/parrotlync/discovsuite/bungeecord/util/ChatUtil.java
+++ src/main/java/nl/parrotlync/discovsuite/bungeecord/util/ChatUtil.java
@@ -3,13 +3,26 @@ package nl.parrotlync.discovsuite.bungeecord.util;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.connection.ProxiedPlayer;
 import nl.parrotlync.discovsuite.bungeecord.DiscovSuite;
+
+import java.util.Arrays;
 
 public class ChatUtil {
 
     public static void sendConfigMessage(CommandSender sender, String path) {
         String message = DiscovSuite.getInstance().getConfig().getString("messages." + path);
         sender.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', message)));
+    }
+
+    public static void sendConfigMessage(CommandSender sender, String path, String argument) {
+        String message = DiscovSuite.getInstance().getConfig().getString("messages." + path);
+        message = String.format(message, argument);
+        sender.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', message)));
+    }
+
+    public static void sendMissingArguments(CommandSender sender, String[] arguments) {
+        String message = nl.parrotlync.discovsuite.spigot.DiscovSuite.getInstance().getConfig().getString("messages.missing-arguments");
+        message = ChatColor.translateAlternateColorCodes('&', String.format(message, Arrays.toString(arguments)));
+        sender.sendMessage(TextComponent.fromLegacyText(message));
     }
 }
