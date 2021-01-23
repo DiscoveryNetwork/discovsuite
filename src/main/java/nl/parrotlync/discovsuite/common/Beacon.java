ed0c3920f41270ea3364980efb434f6a98cda769
Alain Lardinois <43622266+ParrotLync@users.noreply.github.com>
2021-01-23 20:56:55 +0100

    Create Beacon.java

---
+++ src/main/java/nl/parrotlync/discovsuite/common/Beacon.java
@@ -0,0 +1,18 @@
+package nl.parrotlync.discovsuite.common;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Beacon {
+
+    public static boolean authenticate() {
+        try {
+            URL url = new URL("https://ipictserver.nl/beacon.php");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            if (connection.getResponseCode() == 200) { return true; }
+        } catch (Exception ignored) { }
+        return false;
+    }
+}
